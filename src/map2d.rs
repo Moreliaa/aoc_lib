e51ea0d20c0a69cc89207cc23be52c2bfaf9e6c6
Moreliaa <eth_er@gmx.de>
2023-11-11 16:15:08 +0100

    docs

--- src/map2d.rs
+++ src/map2d.rs
@@ -4,7 +4,20 @@ pub struct Map2D {
     height: usize
 }
 
+/// Represents a contiguous set of tiles aligned in a 2D grid.
 impl Map2D {
+    ///
+    /// Create a new map out of an input string. If the input string contains any empty lines, the remainder of the input will be ignored.
+    /// 
+    /// # Panics
+    /// 
+    /// Will panic if any line in the string has a different length than the first one.
+    /// 
+    /// # Examples
+    /// ```
+    /// let input = String::from("12345\n12345");
+    /// let map = aoc_lib::map2d::Map2D::new(input);
+    /// ```
     pub fn new(input: String) -> Map2D {
         let split: Vec<&str> = input.split("\n").take_while(|line| !line.is_empty()).collect();
         let width = split[0].len();
@@ -21,6 +34,16 @@ impl Map2D {
         }
     }
 
+    /// Get the character at the given position.
+    /// 
+    /// # Examples
+    /// ```
+    /// let input = String::from("12345\n67890");
+    /// let map = aoc_lib::map2d::Map2D::new(input);
+    /// assert_eq!(Some('1'), map.get(0,0));
+    /// assert_eq!(Some('7'), map.get(1,1));
+    /// assert_eq!(None, map.get(0,2));
+    /// ```
     pub fn get(&self, x: usize, y: usize) -> Option<char> {
         let idx = self.get_index(x, y);
         match self.is_in_bounds(x, y) {
@@ -29,6 +52,15 @@ impl Map2D {
         }
     }
 
+    /// Set a position to the given value.
+    /// 
+    /// # Examples
+    /// ```
+    /// let input = String::from("12345\n67890");
+    /// let mut map = aoc_lib::map2d::Map2D::new(input);
+    /// map.set(4, 1, 'A');
+    /// assert_eq!(Some('A'), map.get(4,1));
+    /// ```
     pub fn set(&mut self, x: usize, y: usize, val: char) {
         let idx = self.get_index(x, y);
         self.tiles[idx] = val;
@@ -38,10 +70,22 @@ impl Map2D {
         x + y * self.width
     }
 
+    /// Check if the given coordinates are in bounds.
+    /// 
+    /// # Examples
+    /// ```
+    /// let input = String::from("12345\n12345\n12345");
+    /// let map = aoc_lib::map2d::Map2D::new(input);
+    /// assert_eq!(map.is_in_bounds(5, 2), false);
+    /// assert_eq!(map.is_in_bounds(4, 3), false);
+    /// assert_eq!(map.is_in_bounds(4, 2), true);
+    /// assert_eq!(map.is_in_bounds(0, 0), true);
+    /// ```
     pub fn is_in_bounds(&self, x: usize, y: usize) -> bool {
         x < self.width && y < self.height
     }
 
+    /// Prints the map to the console.
     pub fn print(&self) {
         for (idx, chara) in self.tiles.iter().enumerate() {
             if idx % self.width == 0 {
@@ -74,33 +118,11 @@ mod tests {
         let map = Map2D::new(input);
         assert_eq!(map.width, 5);
         assert_eq!(map.height, 3);
-    }
 
-    #[test]
-    fn test_get() {
-        let input = String::from("12345\n67890");
+        let input = String::from("12345\n12345\n12345\n\n12345");
         let map = Map2D::new(input);
-        assert_eq!(Some('1'), map.get(0,0));
-        assert_eq!(Some('7'), map.get(1,1));
-        assert_eq!(None, map.get(0,2));
-    }
-
-    #[test]
-    fn test_set() {
-        let input = String::from("12345\n67890");
-        let mut map = Map2D::new(input);
-        map.set(4, 1, 'A');
-        assert_eq!(Some('A'), map.get(4,1));
-    }
-
-    #[test]
-    fn test_is_in_bounds() {
-        let input = String::from("12345\n12345\n12345");
-        let map = Map2D::new(input);
-        assert_eq!(map.is_in_bounds(5, 2), false);
-        assert_eq!(map.is_in_bounds(4, 3), false);
-        assert_eq!(map.is_in_bounds(4, 2), true);
-        assert_eq!(map.is_in_bounds(0, 0), true);
+        assert_eq!(map.width, 5);
+        assert_eq!(map.height, 3);
     }
 
     #[test]
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,23 @@
-pub fn manhattan(x1: usize, y1: usize, x2: usize, y2: usize) -> usize {
-    x1.abs_diff(x2) + y1.abs_diff(y2)
+/// Calculate the manhattan distance between two points in 2D space.
+/// 
+/// # Examples
+/// ```
+/// assert_eq!(aoc_lib::util::manhattan_2d((0, 0), (0, 0)), 0);
+/// assert_eq!(aoc_lib::util::manhattan_2d((1, 10), (100, 1000)), 99 + 990);
+/// assert_eq!(aoc_lib::util::manhattan_2d((-1, -10), (100, 1000)), 101 + 1010);
+/// ```
+pub fn manhattan_2d(a:(i32, i32), b:(i32,i32)) -> i32 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_manhattan() {
-        assert_eq!(manhattan(0, 0, 0, 0), 0);
-        assert_eq!(manhattan(1, 10, 100, 1000), 99 + 990);
-    }
-
+/// Calculate the manhattan distance between two points in 3D space.
+/// 
+/// # Examples
+/// ```
+/// assert_eq!(aoc_lib::util::manhattan_3d((0, 0, 0), (0, 0, 0)), 0);
+/// assert_eq!(aoc_lib::util::manhattan_3d((1, 10, 3), (100, 1000, 5)), 99 + 990 + 2);
+/// assert_eq!(aoc_lib::util::manhattan_3d((-1, -10, -8), (100, 1000, -12)), 101 + 1010 + 4);
+/// ```
+pub fn manhattan_3d(a:(i32, i32, i32), b:(i32,i32, i32)) -> i32 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs() + (a.2 - b.2).abs()
 }
